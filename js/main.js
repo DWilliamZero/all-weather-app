1ecff9a9badc84fa282b5b73c26afe456b9f8447
DWilliamZero <william.zero@gigkloud.com>
2020-02-04 10:57:21 -0500

    adding share buttons and modals

--- js/main.js
+++ js/main.js
@@ -44,23 +44,25 @@ const renderWeather = function (weather) {
     let cloud = weather.data.list[i].weather[0].description;
     let speed = weather.data.list[i].wind.speed;
     let direction = weather.data.list[i].wind.deg;
+    let norm = i / 8 + 1;     //normalizes the values of day from 1 to 5
 
     let cardinal = degsToCard(direction);  //returns cardinal heading conversion
 
-    let day = weekDay[today + (i / 8 + 1)];    //convert day to words for each ith-day
+    let day = weekDay[today + (i / 8)];    //convert day to words for each ith-day
 
     let newDiv = document.createElement('div');  //create new divs each forecast
 
-    newDiv.className = `day-${i / 8 + 1}`;  // give each day a unique class name (for styling later)
+    newDiv.className = `day-${norm}`;  // give each div a unique class name (for styling later)
 
     if (i === 0) {
-      newDiv.innerHTML = `<h3>${cloud}</h3><h1>${Math.floor(temp)}&#8457;</h1><h3>Wind: ${cardinal} @ ${Math.floor(speed)}-KTS</h3>`
+      newDiv.innerHTML = `<h3 class='today-cloud'>${cloud}</h3><h1 class='today-temp'>${Math.floor(temp)}&#8457;</h1><h3 class='today-wind'>Wind: ${cardinal} @ ${Math.floor(speed)}-KTS</h3><button id="myBtn-today">Share</button>`
       document.querySelector(".today").append(newDiv)
     } else {
-      newDiv.innerHTML = `<h1>${day}</h1><h3>${cloud}</h3><h1>${Math.floor(temp)}&#8457;</h1><h3>Wind: ${cardinal} @ ${Math.floor(speed)}-KTS</h3>`
-      document.querySelector(".nextFour").append(newDiv)
+      newDiv.innerHTML = `<h1 class='h1-day-${norm}'>${day}</h1><h3 class='h3-cloud-${norm}'>${cloud}</h3><h1 class='h1-temp-${norm}'>${Math.floor(temp)}&#8457;</h1><h3 class='h3-wind-${norm}'>Wind: ${cardinal} @ ${Math.floor(speed)}-KTS</h3><button id="myBtn-${norm}">Share</button>`
+      document.querySelector(".nextFour").append(newDiv);
     }
   }
+  modalFunc();
 }
 
 let fiveDay = async function () {
@@ -68,6 +70,9 @@ let fiveDay = async function () {
     const zipCode = document.querySelector('#zip').value;
     const weather = await axios.get(`${BASE_URL}${zipCode},us&units=imperial&APPID=${API_KEY}`)
       .then(weather => {
+        document.querySelector('.city').innerHTML = '';      //ensure .city is clear
+        document.querySelector('.today').innerHTML = '';     //ensure .today is clear
+        document.querySelector('.nextFour').innerHTML = '';  //ensure .nextFour is clear
         renderWeather(weather);
         //console.log(weather);
       })
@@ -77,3 +82,57 @@ let fiveDay = async function () {
   })
 }
 fiveDay();
+
+
+
+///////////////////////////////////////
+/////////                  ///////////
+////////   Welcome To     ///////////
+///////    Modal Town    ///////////
+//////                  ///////////
+//////////////////////////////////
+
+const modalFunc = function () {
+
+  let modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  let btnToday = document.getElementById(`myBtn-today`);
+  let btn2 = document.getElementById(`myBtn-2`);
+  let btn3 = document.getElementById(`myBtn-3`);
+  let btn4 = document.getElementById(`myBtn-4`);
+  let btn5 = document.getElementById(`myBtn-5`);
+
+
+  // Get the <span> element that closes the modal
+  let span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal
+  btnToday.onclick = function () {
+    modal.style.display = "block";
+  }
+  btn2.onclick = function () {
+    modal.style.display = "block";
+  }
+  btn3.onclick = function () {
+    modal.style.display = "block";
+  }
+  btn4.onclick = function () {
+    modal.style.display = "block";
+  }
+  btn5.onclick = function () {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
